8e0f717840b4af05504194ab397324bc00321685
Furqan Ahmad <furqanahmad21@hotmail.com>
2020-09-22 20:29:04 +0500

    Registered the servlet in spring boot
    There are multiple ways to register a servlet in spring boot one is to use @WebServlet and @ServletComponentScan annotations

--- src/main/java/com/furqan/hapi/fhir/HapiServerDemoApplication.java
+++ src/main/java/com/furqan/hapi/fhir/HapiServerDemoApplication.java
@@ -2,12 +2,18 @@ package com.furqan.hapi.fhir;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class HapiServerDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HapiServerDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HapiServerDemoApplication.class, args);
+    }
 
+//    @Bean
+//    ServletRegistrationBean servletRegistrationBean() {
+//        return new ServletRegistrationBean(new HapiFhirServer(), "/fhir/r4/*");
+//    }
 }
--- src/main/java/com/furqan/hapi/fhir/configuration/HapiFhirServer.java
+++ src/main/java/com/furqan/hapi/fhir/configuration/HapiFhirServer.java
@@ -2,18 +2,27 @@ package com.furqan.hapi.fhir.configuration;
 
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.server.RestfulServer;
+import com.furqan.hapi.fhir.provider.PatientProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 
+@WebServlet(urlPatterns = "/fhir/r4/*")
 public class HapiFhirServer extends RestfulServer {
 
-    public HapiFhirServer(){
-        super(FhirContext.forR4());
+    @Autowired
+    PatientProvider patientProvider;
+
+
+    public HapiFhirServer(FhirContext fhirContext) {
+        super(fhirContext);
     }
 
     @Override
     protected void initialize() throws ServletException {
         super.initialize();
-
+        registerProvider(patientProvider);
     }
 }
